03ee02af7bb85b9363b94874cfc8bc6a38f3c70e
Exel Boy Alfanso <72937148+ExelAlfanso@users.noreply.github.com>
2025-07-28 09:17:18 +0700

    fix: tech stack padding and experience text alignment

--- src/pages/Components/Sections/TechStack.tsx
+++ src/pages/Components/Sections/TechStack.tsx
@@ -14,7 +14,7 @@ const TechStack: React.FC<TechStackProps> = ({ id, className }) => {
       <Header className="font-bold mb-10" size="subtitle">
         TECH STACK
       </Header>
-      <div className="flex items-center justify-center bg-primary/50">
+      <div className="flex items-center justify-center py-10 bg-primary/50">
         <div className="grid grid-cols-2 lg:grid-cols-3 gap-10 py-5 px-5 ">
           {techStackDatas.map((item) => (
             <div key={item.id}>
