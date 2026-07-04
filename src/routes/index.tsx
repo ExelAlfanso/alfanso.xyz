dc815dc156bc2bc66d3cab735ad697dd07577356
ExelAlfanso <72937148+ExelAlfanso@users.noreply.github.com>
2026-07-04 21:49:12 +0700

    fix: remove parallax from route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home";
-import Parallax from "../pages/parallax";
 export const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
-      <Route element={<Parallax />} path="/parallax" />
       <Route element={<Home />} path="/" />
     </Routes>
   </BrowserRouter>
